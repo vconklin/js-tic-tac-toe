d604943b887e4b51b0a979de3ff6a91829e896fe
Valerie <val.conklin@gmail.com>
2016-06-30 16:05:42 -0700

    A player can now win by getting three consecutive row spaces

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -18,12 +18,12 @@ TicTacToe.prototype = {
     //the space has to be untaken in order to claim it
     if (this.untaken.includes(space)) {
       var index = this.untaken.indexOf(space)
-      var taken_space = this.untaken.splice(index, 1)
+      var taken_space = this.untaken.splice(index, 1)[0]
       // add this space to the list of spots already taken by the current player
       if (this._turn === 'x') {
         this.taken_by_x.push(taken_space)
         console.log("taken by x: " + this.taken_by_x)
-      } else if (this._turn === 'o') {
+      } else {
         this.taken_by_o.push(taken_space)
         console.log("taken by o: " + this.taken_by_o)
       }
@@ -34,13 +34,51 @@ TicTacToe.prototype = {
   },
 
   //determines if current player has won or not
-  is_win: function () {
+  is_win: function (callback) {
     //matches any of the following win condition examples
       // a1, a2, a3 (all 3 letters are the same)
       // a1, b1, c1 (all 3 numbers are the same)
       // a1, b2, b3 (numbers are 1, 2, 3)
 
-    
+    //win conditions:
+    // if (this.threeSameNumbers() || this.threeSameLetters() || this.threeSequentialNumbers()) {
+    if (this.threeSameNumbers()) {
+      callback(true, this._turn)
+    } else {
+      callback(false, this._turn)
+    }
+  },
+
+  // win condition: a1, b1, c1...etc.
+  threeSameNumbers: function () {
+    // sets up which player's spaces we're talking about
+    var taken_by_player
+    if (this._turn === 'x') {
+      taken_by_player = this.taken_by_x
+      console.log(taken_by_player)
+    } else if (this._turn === 'o') {
+      taken_by_player = this.taken_by_o
+    }
+
+    //counts how many times each row number appears
+    var ones = 0,
+        twos = 0,
+        threes = 0
+    for (var space of taken_by_player) {
+      var row_number = space[1]
+      // e.g., the number '2' in 'a2'
+      if (row_number === '1') {
+        ones++
+      } else if (row_number === '2') {
+        twos++
+      } else if (row_number === '3') {
+        threes++
+      }
+      //if the player has claimed 3 spaces with the same row number, they win!
+      if (ones === 3 || twos === 3 || threes === 3) {
+        return true
+      }
+    }
   }
 
 }
